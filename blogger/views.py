4d45f639e4fc102c2cc22fc540dec4cc50f3cc41
ambition <zongzhong603@gmail.com>
2020-10-10 11:18:44 +0800

    编写detail函数，详情页模版

--- blogger/views.py
+++ blogger/views.py
@@ -9,3 +9,8 @@ from .models import Post
 def index(request):
     post_list = Post.objects.all().order_by('-created_time')
     return render(request, 'blogger/index.html', context={'post_list': post_list})
+
+
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blogger/detail.html', context={'post': post})
