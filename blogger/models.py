1879feb43e379f56fd4890bf6ef300514def6da0
ambition <zongzhong603@gmail.com>
2020-10-12 23:34:04 +0800

    详情页完成学习

--- blogger/models.py
+++ blogger/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from  django.urls import  reverse
 # Create your models here.
 
 
@@ -49,3 +50,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blogger:detail', kwargs={'pk': self.pk})
